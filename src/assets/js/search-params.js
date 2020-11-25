f842972062b9a3b7972adb5987b11bcd64a5ff18
Sean McPherson <git@seanmcp.com>
2020-11-24 20:07:43 -0500

    feat: add count, incorrect options

--- src/assets/js/search-params.js
+++ src/assets/js/search-params.js
@@ -1,14 +1,16 @@
 import { decode } from "./code.js";
 
-// ?n=2&n3&key=116-101-115-116
+// ?c=10&i=true&o=a&n=2&n3&key=116-101-115-116
 export function get() {
     const usp = new URLSearchParams(location.search)
     const numbers = usp.getAll('n').map(Number)
     const operations = usp.getAll('o')
     
     return {
+        allowIncorrect: usp.get('i'),
+        count: usp.get('c'),
         key: decode(usp.get('key')),
+        numbers,
         operations,
-        numbers
     }
 }
