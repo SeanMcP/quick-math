40c0b8c17fca28a2b04437498d9cd052f04595e2
Sean McPherson <git@seanmcp.com>
2020-12-15 07:02:17 -0500

    feat: allow practice exercise

--- docs/assets/js/done.js
+++ docs/assets/js/done.js
@@ -2,8 +2,20 @@ import { get, KEY } from "./storage.js";
 
 (function main() {
   // Code
+  const outputEl = document.getElementById("output");
   const correct = get(KEY.count);
   const satisfactory = get(KEY.satisfactory);
-  document.getElementById("key").textContent =
-    correct >= satisfactory ? get(KEY.pass) : get(KEY.fail);
+  const failKey = get(KEY.fail);
+  const passKey = get(KEY.pass);
+
+  if (passKey && failKey) {
+    outputEl.innerHTML = `
+      <p>Great practicing! Share this key with your teacher:</p>
+      <p style="font-size:5rem;font-weight:bold;text-align:center;">
+        ${correct >= satisfactory ? passKey : failKey}
+      </p>
+    `;
+  }
+
+  outputEl.removeAttribute("hidden");
 })();
